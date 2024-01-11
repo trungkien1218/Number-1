86608b018683effeaa32802045fe2fba6e58aba6
trungkien1218 <136812461+trungkien1218@users.noreply.github.com>
2024-01-11 10:48:22 +0700

    update

---
+++ main.js
@@ -0,0 +1,10 @@
+let hamberger = document.querySelector('.btn-hamber');
+let btnDisplayBlock = document.querySelector('.btn');
+let btn = document.querySelector('.btn-primary');
+let ul = document.querySelector('ul.option.btn')
+
+hamberger.addEventListener('click', function () {
+    hamberger.classList.toggle(btn).inner
+});
+
+hamberger.appendChild(ul);
