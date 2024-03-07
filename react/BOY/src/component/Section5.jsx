a246aa3a1ad2543f6b156275c01ad45e6d58a098
trungkien1218 <136812461+trungkien1218@users.noreply.github.com>
2024-03-07 09:54:56 +0700

    qưeq

--- react/BOY/src/component/Section5.jsx
+++ react/BOY/src/component/Section5.jsx
@@ -3,13 +3,13 @@ import './main.css/'
 const Section5 = () => {
   return (
    < >
-   <div style={{backgroundColor:'black', color:'white'}}>
+   <div style={{backgroundColor:'black', color:'white' }}>
      <div className="container" >
             <div className='text12-sec5'><span class="text111-sec1">My</span> <span
                     className="text222-sec1">Projects</span>
             </div>
             <div className="layout-sec5">
-                <img src="img/MP_photo1.png" alt="" class=""/>
+                <img src="img/MP_photo1.png" alt=""/>
                 <div className="text4-sec5 center-doc">
                     <p className="text22-sec5 text1-sec5">01</p>
                     <p className="text1-sec5 text2-sec5">Crypto Screener Application</p>
@@ -21,8 +21,8 @@ const Section5 = () => {
                 </div>
             </div>
             <div className="grid1-sec5 ">
-                <img src="img/MP_photo2.png" alt="" class="on-sec5"/>
-                <div className=" text4-sec5 center-doc ">
+                <img src="img/MP_photo2.png" alt="" className='on-sec5' />
+                <div className="text4-sec5 center-doc">
                     <p className="text22-sec5 text1-sec5">02</p>
                     <p className="text1-sec5 text2-sec5">Euphoria - Ecommerce (Apparels) Website Template</p>
                     <p className="text1-sec5 text3-sec5">I'm Evren Shah Lorem Ipsum is simply dummy text of the printing and
